61d83827cde505fbcd1870861c96d853be52dcbe
gongza <fengyi036@163.com>
2015-06-09 01:16:24 +0800

    增加volley的一些自定义Request用法

---
+++ src/com/gongza/novice/volleydemo/volleyrequest/StringRequestG.java
@@ -0,0 +1,60 @@
+package com.gongza.novice.volleydemo.volleyrequest;
+
+import java.io.UnsupportedEncodingException;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+/**
+ * 这是一个volley的StringRequest原型，我们的XXXRequest就是仿照它写的
+ * 
+ * A canned request for retrieving the response body at a given URL as a String.
+ */
+public class StringRequestG extends Request<String> {
+    private final Listener<String> mListener;
+
+    /**
+     * Creates a new request with the given method.
+     *
+     * @param method the request {@link Method} to use
+     * @param url URL to fetch the string at
+     * @param listener Listener to receive the String response
+     * @param errorListener Error listener, or null to ignore errors
+     */
+    public StringRequestG(int method, String url, Listener<String> listener,
+            ErrorListener errorListener) {
+        super(method, url, errorListener);
+        mListener = listener;
+    }
+
+    /**
+     * Creates a new GET request.
+     *
+     * @param url URL to fetch the string at
+     * @param listener Listener to receive the String response
+     * @param errorListener Error listener, or null to ignore errors
+     */
+    public StringRequestG(String url, Listener<String> listener, ErrorListener errorListener) {
+        this(Method.GET, url, listener, errorListener);
+    }
+
+    @Override
+    protected void deliverResponse(String response) {
+        mListener.onResponse(response);
+    }
+
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String parsed;
+        try {
+            parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+        } catch (UnsupportedEncodingException e) {
+            parsed = new String(response.data);
+        }
+        return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
+    }
+}
---
+++ src/com/gongza/novice/volleydemo/volleyrequest/Weather.java
@@ -0,0 +1,21 @@
+package com.gongza.novice.volleydemo.volleyrequest;
+
+
+/**
+ * Weather类中只是引用了WeatherInfo
+ * @author gongza
+ *
+ */
+public class Weather {
+
+	private WeatherInfo weatherinfo;
+
+	public WeatherInfo getWeatherinfo() {
+		return weatherinfo;
+	}
+
+	public void setWeatherinfo(WeatherInfo weatherinfo) {
+		this.weatherinfo = weatherinfo;
+	}
+
+}
