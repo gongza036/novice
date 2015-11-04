1d0e1936fdd6e485ebb8e18ca5162e213120c9ef
gongza <fengyi036@163.com>
2015-11-04 17:03:31 +0800

    相机相册裁剪整理

--- src/com/gongza/novice/activity/CameraAct.java
+++ src/com/gongza/novice/activity/CameraAct.java
@@ -38,8 +38,8 @@ public class CameraAct extends Activity implements OnClickListener {
 
 	// private File tempFile;
 
-	private File tempFile = new File(Environment.getExternalStorageDirectory(),
-			getPhotoFileName());
+	private File tempFile;
+	private File cropFile;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -77,31 +77,68 @@ public class CameraAct extends Activity implements OnClickListener {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		switch (requestCode) {
-		case CROP_SMALL:
+		case PICK:
+			// 相册回来 --- 此时data中是所选图片的uri地址
 			if (data != null) {
 				Uri uri = data.getData();
 				iv_camera.setImageBitmap(getBitmapFromUri(uri));
 			}
 			break;
-		case CROP_LARGE:
-
-			break;
 		case CAMERA_SMALL:
+			// 相机小图回来 ---- 此时data中包含图片对象
 			if (resultCode == RESULT_OK) {
 				Bitmap photo = data.getParcelableExtra("data");
 				if (photo != null) {
 					iv_camera.setImageBitmap(photo);
+					tv_camera.setText("");
 				}
 			}
 
 			break;
 		case CAMERA_LARGE:
+			// 相机大图回来 --- 此时图片已经保存在指定File文件中
 			if (resultCode == RESULT_OK) {
-				Drawable imageDrawable = Drawable.createFromPath(tempFile
-						.toString());
-				BitmapDrawable bd = (BitmapDrawable) imageDrawable;
-				Bitmap bm = bd.getBitmap();
-				iv_camera.setImageBitmap(bm);
+				// Drawable imageDrawable = Drawable.createFromPath(tempFile
+				// .toString());
+				// BitmapDrawable bd = (BitmapDrawable) imageDrawable;
+				// Bitmap bm = bd.getBitmap();
+				// iv_camera.setImageBitmap(bm);
+				iv_camera.setImageBitmap(getBitmapFromFile(tempFile));
+			}
+			break;
+		case CROP_PICK:
+			// 相册回来进裁剪
+			if (resultCode == RESULT_OK) {
+				Uri uri = data.getData();
+				IntentCropFromPick(uri);
+			}
+			break;
+		case CROP_CAMERA:
+			// 相机回来进裁剪大图
+			if (resultCode == RESULT_OK) {
+				IntentCropFromCamera();
+			}
+			break;
+		case CROP:
+			// 裁剪大图回来
+			if (resultCode == RESULT_OK) {
+				iv_camera.setImageBitmap(getBitmapFromFile(cropFile));
+			}
+			break;
+		case CROP_CAMERA_SMALL:
+			// 相机回来进裁剪小图
+			if (resultCode == RESULT_OK) {
+				IntentCropSmallFromCamera();
+			}
+			break;
+		case CROP_SMALL:
+			// 裁剪小图回来
+			if (resultCode == RESULT_OK) {
+				Bitmap photo = data.getParcelableExtra("data");
+				if (photo != null) {
+					iv_camera.setImageBitmap(photo);
+					tv_camera.setText("");
+				}
 			}
 			break;
 
@@ -110,46 +147,128 @@ public class CameraAct extends Activity implements OnClickListener {
 		}
 	}
 
-	private static final int CROP_SMALL = 1001;// 相册小图
-	private static final int CROP_LARGE = 1002;// 相册大图
-	private static final int CAMERA_SMALL = 2001;// 相机
-	private static final int CAMERA_LARGE = 2002;// 相机
+	private static final int PICK = 1001;// 相册
+	private static final int CAMERA_SMALL = 2001;// 相机小图
+	private static final int CAMERA_LARGE = 2002;// 相机大图
+	private static final int CROP_PICK = 3001;// 相册裁剪
+	private static final int CROP_CAMERA = 3002;// 相机裁剪大图
+	private static final int CROP_CAMERA_SMALL = 3003;// 相机裁剪小图
+	private static final int CROP = 4000;// 裁剪大图
+	private static final int CROP_SMALL = 4001;// 裁剪小图
 
+	/** 进相机大图 */
 	private void IntentCameraLarge() {
+		tempFile = new File(Environment.getExternalStorageDirectory(),
+				getPhotoFileName());
 		Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		// 指定调用相机拍照后照片的储存路径
 		cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
 		startActivityForResult(cameraintent, CAMERA_LARGE);
-
 	}
 
+	/** 进相机小图 */
 	private void IntentCameraSmall() {
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		startActivityForResult(intent, CAMERA_SMALL);
 	}
 
-	private void IntentCropSmall() {
+	/** 进相册 */
+	private void IntentPick() {
 		Intent intent = new Intent(Intent.ACTION_PICK,
 				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 		intent.setType("image/*");
+		startActivityForResult(intent, PICK);
+	}
+
+	/** 进相册再裁剪 */
+	private void IntentCropPick() {
+		Intent intent = new Intent(Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		intent.setType("image/*");
+		startActivityForResult(intent, CROP_PICK);
+	}
+
+	/** 相册回来再进裁剪 */
+	private void IntentCropFromPick(Uri uri) {
+		cropFile = new File(Environment.getExternalStorageDirectory(),
+				getPhotoFileName());
+		Uri saveUri = Uri.fromFile(cropFile);
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 1);// 裁剪框的比例，1：1
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 800);// 裁剪后输出图片的尺寸大小
+		intent.putExtra("outputY", 800);
+		intent.putExtra("scale", true);
+		intent.putExtra("scaleUpIfNeeded", true);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, saveUri);
+		intent.putExtra("return-data", false); // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_CUT
+		intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());// 图片格式
+		intent.putExtra("noFaceDetection", true); // 取消人脸识别
+		startActivityForResult(intent, CROP);
+	}
+
+	/** 进相机再裁剪大图 */
+	private void IntentCropCamera() {
+		tempFile = new File(Environment.getExternalStorageDirectory(),
+				getPhotoFileName());
+		Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// 指定调用相机拍照后照片的储存路径
+		cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+		startActivityForResult(cameraintent, CROP_CAMERA);
+	}
+
+	/** 相机回来再进裁剪大图 */
+	private void IntentCropFromCamera() {
+		cropFile = new File(Environment.getExternalStorageDirectory(),
+				getPhotoFileName());
+		Uri saveUri = Uri.fromFile(cropFile);
+		Uri uri = Uri.fromFile(tempFile);
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 1);// 裁剪框的比例，1：1
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 800);// 裁剪后输出图片的尺寸大小
+		intent.putExtra("outputY", 800);
+		intent.putExtra("scale", true);
+		intent.putExtra("scaleUpIfNeeded", true);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, saveUri);
+		intent.putExtra("return-data", false); // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_CUT
+		intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());// 图片格式
+		intent.putExtra("noFaceDetection", true); // 取消人脸识别
+		startActivityForResult(intent, CROP);
+	}
+
+	/** 进相机再裁剪小图 */
+	private void IntentCropSmallCamera() {
+		tempFile = new File(Environment.getExternalStorageDirectory(),
+				getPhotoFileName());
+		Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// 指定调用相机拍照后照片的储存路径
+		cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+		startActivityForResult(cameraintent, CROP_CAMERA_SMALL);
+	}
+
+	/** 相机回来再进裁剪 -剪出小图 */
+	private void IntentCropSmallFromCamera() {
+		Uri uri = Uri.fromFile(tempFile);
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		intent.putExtra("crop", "true");
+		intent.putExtra("aspectX", 7);
+		intent.putExtra("aspectY", 4);
+		intent.putExtra("outputX", 350);// 这里不能超过400
+		intent.putExtra("outputY", 200);
+		intent.putExtra("return-data", true);
+		intent.putExtra("noFaceDetection", true);
 		startActivityForResult(intent, CROP_SMALL);
 	}
 
+	/** 打开弹出框 */
 	private void showPopupWindow() {
 		pop = new CameraPopwin(CameraAct.this, new CameraPopupButtonListener() {
-
-			@Override
-			public void cropSmallClick(View v) {
-				// 相册小图
-				pop.dismiss();
-				IntentCropSmall();
-			}
-
-			@Override
-			public void cropLargeClick(View v) {
-				// 相册大图
-				pop.dismiss();
-			}
 
 			@Override
 			public void cameraSmallClick(View v) {
@@ -163,6 +282,34 @@ public class CameraAct extends Activity implements OnClickListener {
 				// 相机大图
 				pop.dismiss();
 				IntentCameraLarge();
+			}
+
+			@Override
+			public void pickClick(View v) {
+				// 相册直接取
+				pop.dismiss();
+				IntentPick();
+			}
+
+			@Override
+			public void pickCropClick(View v) {
+				// 相册裁剪
+				pop.dismiss();
+				IntentCropPick();
+			}
+
+			@Override
+			public void cameraCropClick(View v) {
+				// 相机裁剪大图
+				pop.dismiss();
+				IntentCropCamera();
+			}
+
+			@Override
+			public void cameraCropSmallClick(View v) {
+				// 相机裁剪小图
+				pop.dismiss();
+				IntentCropSmallCamera();
 			}
 		});
 		pop.showAtLocation(CameraAct.this.findViewById(R.id.layout_camera),
@@ -189,7 +336,7 @@ public class CameraAct extends Activity implements OnClickListener {
 
 	}
 
-	// 使用系统当前日期加以调整作为照片的名称
+	/** 使用系统当前日期加以调整作为照片的名称 */
 	private String getPhotoFileName() {
 		Date date = new Date(System.currentTimeMillis());
 		SimpleDateFormat dateFormat = new SimpleDateFormat(
@@ -197,8 +344,9 @@ public class CameraAct extends Activity implements OnClickListener {
 		return dateFormat.format(date) + ".jpg";
 	}
 
-	// uri获取bitmap
+	/** uri获取bitmap */
 	private Bitmap getBitmapFromUri(Uri uri) {
+		tv_camera.setText(uri.toString());
 		try {
 			// 读取uri所在的图片
 			Bitmap bitmap = MediaStore.Images.Media.getBitmap(
@@ -211,4 +359,14 @@ public class CameraAct extends Activity implements OnClickListener {
 			return null;
 		}
 	}
+
+	/** 文件获取bitmap */
+	private Bitmap getBitmapFromFile(File file) {
+		tv_camera.setText(file.getAbsolutePath());
+		Drawable imageDrawable = Drawable.createFromPath(file.toString());
+		BitmapDrawable bd = (BitmapDrawable) imageDrawable;
+		Bitmap bm = bd.getBitmap();
+		return bm;
+	}
+	
 }
--- src/com/gongza/views/popupwindow/CameraPopwin.java
+++ src/com/gongza/views/popupwindow/CameraPopwin.java
@@ -13,8 +13,8 @@ import android.widget.PopupWindow;
 import com.gongza.novice.R;
 
 public class CameraPopwin extends PopupWindow {
-	private Button bt_camera_large, bt_crop_large, bt_camera_small,
-			bt_crop_small, bt_cancel;
+	private Button bt_camera_large, bt_camera_small, bt_pick, bt_pick_crop,
+			bt_camera_crop, bt_camera_crop_small,bt_cancel;
 	private View popView;
 	private CameraPopupButtonListener btnOnClick;
 
@@ -26,9 +26,11 @@ public class CameraPopwin extends PopupWindow {
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		popView = inflater.inflate(R.layout.popwin_camera, null);
 		bt_camera_large = (Button) popView.findViewById(R.id.bt_camera_large);
-		bt_crop_large = (Button) popView.findViewById(R.id.bt_crop_large);
 		bt_camera_small = (Button) popView.findViewById(R.id.bt_camera_small);
-		bt_crop_small = (Button) popView.findViewById(R.id.bt_crop_small);
+		bt_pick = (Button) popView.findViewById(R.id.bt_pick);
+		bt_pick_crop = (Button) popView.findViewById(R.id.bt_pick_crop);
+		bt_camera_crop = (Button) popView.findViewById(R.id.bt_camera_crop);
+		bt_camera_crop_small = (Button) popView.findViewById(R.id.bt_camera_crop_small);
 		bt_cancel = (Button) popView.findViewById(R.id.bt_cancel);
 
 		// 取消按钮
@@ -60,22 +62,43 @@ public class CameraPopwin extends PopupWindow {
 				}
 			}
 		});
-
-		bt_crop_large.setOnClickListener(new OnClickListener() {
+		// 相册
+		bt_pick.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				if (btnOnClick != null) {
-					btnOnClick.cropLargeClick(v);
+					btnOnClick.pickClick(v);
 				}
 			}
 		});
-		bt_crop_small.setOnClickListener(new OnClickListener() {
+		// 相册裁剪
+		bt_pick_crop.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				if (btnOnClick != null) {
-					btnOnClick.cropSmallClick(v);
+					btnOnClick.pickCropClick(v);
+				}
+			}
+		});
+		// 相机裁剪大图
+		bt_camera_crop.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (btnOnClick != null) {
+					btnOnClick.cameraCropClick(v);
+				}
+			}
+		});
+		// 相机裁剪小图
+		bt_camera_crop_small.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (btnOnClick != null) {
+					btnOnClick.cameraCropSmallClick(v);
 				}
 			}
 		});
@@ -94,8 +117,12 @@ public class CameraPopwin extends PopupWindow {
 
 		public void cameraSmallClick(View v);
 
-		public void cropLargeClick(View v);
+		public void pickClick(View v);
 
-		public void cropSmallClick(View v);
+		public void pickCropClick(View v);
+
+		public void cameraCropClick(View v);
+		
+		public void cameraCropSmallClick(View v);
 	};
 }
